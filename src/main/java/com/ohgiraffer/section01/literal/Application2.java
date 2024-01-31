1c565e82e4f7e105bab3b153f1c18c2a36f3e015
pyunghun <564564q@naver.com>
2024-01-31 16:52:57 +0900

    [test2] 123

---
+++ src/main/java/com/ohgiraffer/section01/literal/Application2.java
@@ -0,0 +1,46 @@
+package com.ohgiraffer.section01.literal;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("=========정수와 정수의 연산=============");
+        System.out.println(123+456);
+        System.out.println(123-23);
+        System.out.println(123 * 10);
+        System.out.println(123 / 10);
+        System.out.println(123 % 10);   
+
+        System.out.println(1.23+1.23);
+        System.out.println(1.23 - 0.23);
+        System.out.println(1.23 * 1.23);
+        System.out.println(1.23 / 10.0);
+        System.out.println(1.23 % 10.0);
+
+        System.out.println(123 + 0.5);
+        System.out.println(123 - 0.5);
+        System.out.println(123 * 0.5);
+        System.out.println(123 / 0.5);
+        System.out.println(123 % 0.5);
+
+        System.out.println("=====ab======");
+        System.out.println('a'+'b');
+        System.out.println('a' - 'b');
+        System.out.println('a' * 'b');
+        System.out.println('a' / 'b');
+        System.out.println('a' % 'b');
+        System.out.println("===============");
+
+        System.out.println('안' + '녕');
+        System.out.println(1.23 % 10.0);
+        System.out.println(123 % 0.5);
+        System.out.println('a' );
+        System.out.println("hello" + "world");
+        System.out.println("hello" + 123);
+        System.out.println("hello" + true);
+
+        System.out.println("a"+ 123);
+       
+    }
+
+}
---
+++ src/main/java/com/ohgiraffer/section01/literal/Application3.java
@@ -0,0 +1,15 @@
+package com.ohgiraffer.section01.literal;
+
+public class Application3 {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+
+
+
+
+    }
+
+}
